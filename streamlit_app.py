cf9841f988b9390ba55890f936e425bcd63e9754
Samuel <zamuel@ciencias.unam.mx>
2025-05-12 19:53:12 -0600

    add pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import  pandas as pd
 
 # Write directly to the app
 st.title(f"Smoothie:balloon: {st.__version__}")
