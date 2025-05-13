9643f77772d7820b8042c9e7bdf2e06ec3520ac5
Samuel <zamuel@ciencias.unam.mx>
2025-05-12 19:20:35 -0600

    Update streamlit_app.py
    add requests library

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
