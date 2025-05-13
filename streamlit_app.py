dbfb87132fb4a729df7031daf88c4440b5addcda
Samuel <zamuel@ciencias.unam.mx>
2025-05-12 19:55:16 -0600

    add column

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie  will be", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     "choose up to 5 ingredients",
